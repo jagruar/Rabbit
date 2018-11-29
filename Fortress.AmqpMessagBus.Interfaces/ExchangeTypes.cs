c39821b31050d323039166fbced140d7cca18f80
Charlie Kendall <charlie.kendall@fortressgb.com>
2018-11-29 15:51:49 +0000

    Stuff

---
+++ Fortress.AmqpMessagBus.Interfaces/ExchangeTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace Fortress.MessagBus.Interfaces
+{
+    /// <summary>
+    /// The type of AMQP exchanges.
+    /// </summary>
+    public enum ExchangeTypes
+    {
+        Direct = 1,
+        Fanout = 2,
+        Topic = 3,
+        Headers = 4
+    }
+}
---
+++ Fortress.AmqpMessagBus.Interfaces/IAmqpMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fortress.MessagBus.Interfaces
+{
+    public interface IAmqpMessageBus : IDisposable
+    {
+        void Subscribe(string exchange, string queue, EventHandler<> func, ExchangeTypes exchangeType = ExchangeTypes.Direct);
+        void PublishToAll(string message, string exchangeName, ExchangeTypes exchangeType = ExchangeTypes.Direct);
+        void PublishOnce(string message, string exchangeName, ExchangeTypes exchangeType = ExchangeTypes.Direct);
+    }
+}
